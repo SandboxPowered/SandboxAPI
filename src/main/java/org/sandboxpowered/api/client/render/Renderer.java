3bc782ce5304735d4208474042de035933d22d7c
Coded <coded@voxel.games>
2021-05-17 15:32:27 +0100

    Allow render materials to be registered

--- src/main/java/org/sandboxpowered/api/client/render/Renderer.java
+++ src/main/java/org/sandboxpowered/api/client/render/Renderer.java
@@ -1,5 +1,6 @@
 package org.sandboxpowered.api.client.render;
 
+import org.sandboxpowered.api.client.render.material.RenderMaterial;
 import org.sandboxpowered.api.client.render.model.BakedModel;
 import org.sandboxpowered.api.util.Identifier;
 import org.sandboxpowered.api.world.state.BlockState;
@@ -8,4 +9,8 @@ public interface Renderer {
     Identifier getId();
 
     BakedModel getBlockModel(BlockState state);
+
+    RenderMaterial getRenderMaterial(Identifier id);
+
+    boolean registerRenderMaterial(Identifier id, RenderMaterial material);
 }
