877f4cda8f5787a1c7bab80eec07790a5937b48e
Coded <coded@voxel.games>
2021-06-20 21:26:15 +0100

    Add identifier factory

--- src/main/java/org/sandboxpowered/api/util/Identifier.java
+++ src/main/java/org/sandboxpowered/api/util/Identifier.java
@@ -1,11 +1,27 @@
 package org.sandboxpowered.api.util;
 
+import org.sandboxpowered.api.Sandbox;
+
 public interface Identifier extends Comparable<Identifier> {
+    static Identifier of(String id) {
+        return Sandbox.getFactoryProvider().provide(IdentifierFactory.class).create(id);
+    }
+
+    static Identifier of(String namespace, String path) {
+        return Sandbox.getFactoryProvider().provide(IdentifierFactory.class).create(namespace, path);
+    }
+
     String getNamespace();
 
     String getPath();
 
     interface Variant extends Identifier {
         String getVariant();
+    }
+
+    interface IdentifierFactory {
+        Identifier create(String namespace, String path);
+
+        Identifier create(String id);
     }
 }
